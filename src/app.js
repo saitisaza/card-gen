28b0ce27e48daa06a9166a8051016f169d126dcc
saitisaza <156968617+saitisaza@users.noreply.github.com>
2024-03-05 10:06:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 window.onload = () => {
-
-  document.querySelector('.card') .classList.add(generateRandomSuit());
-  document.querySelector('.card') .innerHTML = generateRandomNumber();
-
- 
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
 
 let generateRandomNumber = () => {
-  let numbers = ["A","2", "3", "4","5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  let indexNumbers = Math.floor(Math.random () * numbers.length);
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
   return numbers[indexNumbers];
 };
 
@@ -16,6 +27,4 @@ let generateRandomSuit = () => {
   let suit = ["diamond", "heart", "spade", "club"];
   let indexSuit = Math.floor(Math.random() * suit.length);
   return suit[indexSuit];
-
 };
-
